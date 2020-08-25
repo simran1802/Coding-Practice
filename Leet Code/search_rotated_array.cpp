ed73f0dd07e99997cd18ffd2e7e8569bb7cd7972
Simran <51385869+simran1802@users.noreply.github.com>
2020-08-25 17:51:59 +0530

    Search in Rotated Sorted Array

---
+++ Leet Code/search_rotated_array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low=0;
+        int high= nums.size()-1;
+        while(low<high){
+            int mid = (low+high)/2;
+            if ((nums[0] > target) ^ (nums[0] > nums[mid]) ^ (target > nums[mid]))
+                low = mid + 1;
+            else
+                high= mid;
+        }
+        return low==high && nums[low]==target ? low : -1;
+    }
+};
