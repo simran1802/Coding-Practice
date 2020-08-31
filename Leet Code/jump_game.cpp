0113f596085d536b44103bb280f335e5813a2ed0
Simran <51385869+simran1802@users.noreply.github.com>
2020-09-01 04:15:30 +0530

    Jump Game

---
+++ Leet Code/jump_game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int target = nums.size()-1;
+        for(int i=nums.size()-1;i>=0;i--)
+            if(nums[i] + i >= target)
+                target = i;
+        return !target;
+    }
+};
