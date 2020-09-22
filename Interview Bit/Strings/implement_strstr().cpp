68a95c2dba51cdee77fc43ab9b4c0d6a8de9d917
Simran <51385869+simran1802@users.noreply.github.com>
2020-09-22 23:55:03 +0530

    Implement StrStr

---
+++ Interview Bit/Strings/implement_strstr().cpp
@@ -0,0 +1,18 @@
+bool isEqual(const string A, const string B, int n, int m, int i) {
+    for(int j = 0; j < m; j ++)
+        if(A[j+i] != B[j])
+            return false;
+    return true;
+}
+
+int Solution::strStr(const string A, const string B) {
+    if(A.size() == 0 || B.size() == 0)
+        return -1;
+    int n = A.size();
+    int m = B.size();
+    for(int i = 0; i < (n-m+1); i ++)
+        if(isEqual(A, B, n, m, i))
+            return i;
+    return -1;
+}
+
