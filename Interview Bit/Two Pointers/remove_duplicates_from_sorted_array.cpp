efddc569e87e6805de79da62ecc72fec58bac5b1
Simran <51385869+simran1802@users.noreply.github.com>
2020-10-05 02:47:28 +0530

    Remove Duplicates from Sorted Array

---
+++ Interview Bit/Two Pointers/remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,17 @@
+int Solution::removeDuplicates(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int n = A.size()-1;
+    while(n>0){
+        if(A[n] == A[n-1]){
+            A.erase(A.begin()+n);
+            
+        }
+        
+        n--;
+        
+    }
+    return A.size();
+}
