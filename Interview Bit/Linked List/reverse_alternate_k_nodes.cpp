40f2f2ba5723f575c905a27169d142e9f980dcda
Simran <51385869+simran1802@users.noreply.github.com>
2020-10-11 02:16:56 +0530

    Reverse Alternate K Nodes

--- Interview Bit/Linked List/reverse_alternate_k_nodes.cpp
+++ Interview Bit/Linked List/reverse_alternate_k_nodes.cpp
@@ -6,39 +6,40 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-ListNode* Solution::solve(ListNode* A, int B) {
-    ListNode *curr=A;
-    ListNode *prev=NULL, *next=NULL;
-    int len=0;
-    static bool alternate = true;
-    if(alternate){
-        while(curr && len<B){
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-            len++;
-        }
-        alternate = false;
-        if(next){
-            A->next = solve(next,B);
-        }
-        
-    }
-    else if(!alternate){
-        prev = A;
-        while(curr && len<B){
-            next = curr;
-            curr = curr->next;
-            len++;
-        }
-        alternate=true;
-        if(next)
-            next->next = solve(next->next,B);
-        
-    }
-    alternate = true;
-    return prev;
+ListNode* Solution::solve(ListNode* A, int b) {
+    if(A == NULL) 
+        return NULL;
     
+    ListNode *head,*curr,*next,*prev;
     
+    curr = A; prev = NULL;
+    int count = 0;
+    
+    while(count < b && curr)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        
+        count++;
+    }
+    
+    head = prev;
+    A->next = curr;
+    
+    if(curr==NULL) 
+        return head;
+    
+    count = 1;
+    
+    while(count < b && curr)
+    {
+        curr = curr->next;
+        count ++;
+    }
+    
+    curr->next = solve(curr->next,b);
+    
+    return head;
 }
