633832821779e628609b8321d1fd11c8b5f20e1c
Simran <51385869+simran1802@users.noreply.github.com>
2020-11-04 03:06:09 +0530

    Equal

---
+++ Interview Bit/Hashing and HashTables/equal.cpp
@@ -0,0 +1,26 @@
+vector<int> Solution::equal(vector<int> &A) {
+    int i=-1, j=-1;
+    unordered_map<int,pair<int,int>> m;
+    vector<vector<int>> res;
+    for(int i=0;i<A.size();i++){
+        for(int j=i+1;j<A.size();j++){
+            if(m.find(A[i]+A[j])==m.end()){
+                m[A[i]+A[j]] = {i,j};
+            }
+            else{
+                int x = m[A[i]+A[j]].first;
+                int y = m[A[i]+A[j]].second;
+                if(x!=i && x!=j && y!=i && y!=j){
+                    vector<int> v;
+                    v.push_back(x);
+                    v.push_back(y);
+                    v.push_back(i);
+                    v.push_back(j);
+                    res.push_back(v);
+                }
+            }
+        }
+    }
+    sort(res.begin(),res.end());
+    return res[0];
+}
