557297230870817d3b09c60532dc91b249713143
Simran <51385869+simran1802@users.noreply.github.com>
2020-11-09 22:41:52 +0530

    Task Scheduler

---
+++ Leet Code/task_schedular.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char,int> mp;
+        int count=0;
+        for(auto i : tasks){
+            mp[i]++;
+            count = max(count, mp[i]);
+        }
+        int ans = (count-1)*(n+1);
+        for(auto i : mp){
+            if(i.second == count)
+                ans++;
+            
+        }
+        return max((int)tasks.size(),ans);
+    }
+};
