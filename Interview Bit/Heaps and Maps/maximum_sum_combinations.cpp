5df120808913357b370acec0087af185a4905626
Simran <51385869+simran1802@users.noreply.github.com>
2020-11-10 19:23:44 +0530

    Maximum Sum Combinations

---
+++ Interview Bit/Heaps and Maps/maximum_sum_combinations.cpp
@@ -0,0 +1,16 @@
+vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) {
+    vector<int>res;
+    priority_queue<tuple<int, int, int>>q;
+    sort(A.rbegin(), A.rend()); 
+    sort(B.rbegin(), B.rend());
+    for(int i=0; i<C; i++) {
+        q.push({A[i]+B[0], i, 0});
+    }
+    while(res.size()<C) {
+        auto [sum, i, j] = q.top();
+        q.pop();
+        res.push_back(sum);
+        q.push({A[i]+B[j+1], i, j+1});
+    }
+    return res;
+}
