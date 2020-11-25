845dfefd5f4733b07bebc189d85dcc964f8d9547
Simran <51385869+simran1802@users.noreply.github.com>
2020-11-26 04:26:36 +0530

    Intersecting Chords in a Circle

--- Interview Bit/Dynamic Programming/intersecting_chords_in_circle.cpp
+++ Interview Bit/Dynamic Programming/intersecting_chords_in_circle.cpp
@@ -1,10 +1,15 @@
 int Solution::chordCnt(int A) {
+    #define N 1000000007
     long long int dp[A+1];
     memset(dp,0,sizeof(dp));
     dp[0]=dp[1]=1;
     for(int i=2;i<=A;i++){
-    for(int j=0;j<i;j++)
-    dp[i]=(dp[i]+(dp[j]*dp[i-1-j])%1000000007)%1000000007;
+        int left=0, right=i-1;
+        while(left<=i-1){
+            dp[i] = (dp[i]+(dp[left]*dp[right])%N)%N;
+            left++;
+            right--;
+        }
     }
     return dp[A];
 }
