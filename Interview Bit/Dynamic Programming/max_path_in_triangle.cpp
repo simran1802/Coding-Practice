0b23622cc7b7f64c1f91136249fb49eee16e6a68
Simran <51385869+simran1802@users.noreply.github.com>
2020-12-03 19:13:01 +0530

    Maximum Path in Triangle

---
+++ Interview Bit/Dynamic Programming/max_path_in_triangle.cpp
@@ -0,0 +1,8 @@
+int Solution::solve(vector<vector<int> > &A) {
+    for(int i=A.size()-2;i>=0;i--){
+        for(int j=0;j<i+1;j++){
+            A[i][j] += max(A[i+1][j+1],A[i+1][j]);
+        }
+    }
+    return A[0][0];
+}
