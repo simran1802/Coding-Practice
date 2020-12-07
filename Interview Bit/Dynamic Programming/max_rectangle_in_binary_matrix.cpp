29fef3c8c103f4ea85be2da3ff8e8d3341c10a09
Simran <51385869+simran1802@users.noreply.github.com>
2020-12-08 01:34:30 +0530

    Max Rectangle in Binary Matrix

---
+++ Interview Bit/Dynamic Programming/max_rectangle_in_binary_matrix.cpp
@@ -0,0 +1,47 @@
+int Solution::maximalRectangle(vector<vector<int> > &matrix) {
+    if(matrix.empty())
+            return 0; 
+        const int m = matrix.size(), n = matrix[0].size();
+        int left[n], right[n], height[n];
+        fill_n(left,n,0);
+        fill_n(right,n,n);
+        fill_n(height,n,0);
+        int max_area = 0;
+        
+        for(int i=0;i<m;i++){
+            
+            // Compute height
+            int cur_left=0, cur_right=n;
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == 1)
+                    height[j]++;
+                else
+                    height[j] = 0;
+                
+            }
+            // Compute left
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == 1)
+                    left[j] = max(left[j],cur_left);
+                else{
+                    left[j] = 0;
+                    cur_left=j+1;
+                }
+            }
+            
+            // Compute right
+            for(int j=n-1;j>=0;j--){
+                if(matrix[i][j] == 1)
+                    right[j] = min(cur_right,right[j]);
+                else{
+                    right[j] = n;
+                    cur_right = j;
+                }
+            }
+            
+            for(int j=0;j<n;j++){
+                max_area = max(max_area,(right[j]-left[j])*height[j]);
+            }
+        }
+        return max_area;
+}
