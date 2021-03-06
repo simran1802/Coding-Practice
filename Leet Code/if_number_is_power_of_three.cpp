946f10d758003885a07b2277432e6fc151db4e2a
Simran <51385869+simran1802@users.noreply.github.com>
2021-03-06 22:50:20 +0530

    Check if Number is a Sum of Powers of Three

---
+++ Leet Code/if_number_is_power_of_three.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while(n>1 && (n%3==0 || (n-1)%3==0)){
+            if(n%3==0)
+                n/=3;
+            else
+                n--;
+        }
+        return n==1;
+    }
+};
